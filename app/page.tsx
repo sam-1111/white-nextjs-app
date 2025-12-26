44717d79042f52ecf63f3f3f72ca32f26eadc288
Samkumaar.D.N <samkumarsam14@gmail.com>
2025-12-26 16:50:37 +0530

    trigger first amplify deployment

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ export default function Home() {
     <div style={{
       width: "100vw",
       height: "100vh",
-      backgroundColor: "white"
+      backgroundColor: "green"
     }}>
     </div>
   );
 }
+// first amplify deploy
